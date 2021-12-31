a44f6c36cb9161fc85655a01cab3318c3c603a64
Mathurshan Nadarajar <mathunada07@gmail.com>
2021-12-30 20:35:33 -0500

    Initial commit

---
+++ background.js
@@ -0,0 +1,8 @@
+const start = Date.now();
+var date1 = new Date(2022, 0, 2);
+console.log(date1.getTime()-start);
+
+function createAlarm() {
+    var alarmLink = document.getElementById("link-input").value
+    var alarmDate = document.getElementById("selected-time").value
+}
