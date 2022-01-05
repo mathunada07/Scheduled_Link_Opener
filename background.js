72708bd287c8a73d6635873d631707cfd6d728b0
Mathurshan Nadarajar <mathunada07@gmail.com>
2022-01-05 08:45:36 -0500

    Added remove button, created visual representation of link alarms
    Used javascript to create a list that presents all links with their scheduled times. Also added a button to each item of the list to remove scheduled links.  The list updates when a link is added, removed, when all alarms are cleared and when the extension reopens. Created a function that adds a hash mark after a URL along with a number to differentiate from identical URLs to bypass the limitation of alarms having unique names/links in chrome.alarms API.

--- background.js
+++ background.js
@@ -1,14 +1,28 @@
 chrome.runtime.onMessage.addListener(function (message) {
-    console.log("got called");
-    console.log(message);
-    if (message.link != "" && message.time != null) {
-        var alarmLink = message.link;
+    if (message == "clear") {
+        chrome.alarms.clearAll();
+    }
+    else if (message == "loaded") {
+        var alarmsArray = chrome.alarms.getAll();
+        alarmsArray.then(function (result) {
+            for (let i = 0; i < result.length; i++) {
+                console.log("processing");
+                var alarmLink = result[i].name;
+                var alarmDate = new Date(result[i].scheduledTime);
+                chrome.runtime.sendMessage({ link: alarmLink, date: alarmDate.toLocaleString("en-CA")});
+            }
+        });
+    }
+    var alarmLink = message.link;
+    if (message.remove == true) {
+        chrome.alarms.clear(alarmLink);
+    }
+    else if (message.remove == false) {
         var alarmTime = message.time;
         chrome.alarms.create(
             alarmLink, {
             when: alarmTime
         });
-        console.log("alarm created!");
     }
 });
 
@@ -16,5 +30,5 @@ chrome.alarms.onAlarm.addListener(openLink);
 
 function openLink(alarm) {
     chrome.tabs.create({ url: alarm.name });
-    console.log("new tab created!");
+    chrome.alarms.clear(alarm.name);
 }
--- popup.js
+++ popup.js
@@ -1,7 +1,13 @@
-const start = new Date();
 window.addEventListener("load", initializeExtension());
 
 function initializeExtension() {
+    restrictTime();
+
+    chrome.runtime.sendMessage("loaded");
+}
+
+function restrictTime() {
+    const start = new Date();
     var year = start.getFullYear();
     var month = start.getMonth()+1;
     var day = start.getDate();
@@ -32,10 +38,76 @@ function initializeExtension() {
 
 document.getElementById("alarm-form").addEventListener("submit", function(event) {
     event.preventDefault();
-    var alarmLink = document.getElementById("link-input").value;
+    var alarmLink = generateUniqueLink(document.getElementById("link-input").value);
     var alarmDate = new Date(document.getElementById("selected-time").value);
-    alert(alarmLink);
-    console.log(alarmDate.getTime());
-    chrome.runtime.sendMessage({link: alarmLink, time: alarmDate.getTime()});
-    console.log("alarm sent!");
+    document.getElementById("link-input").value = "";
+    document.getElementById("selected-time").value = "";
+
+
+    createListItem(alarmLink, alarmDate.toLocaleString("en-CA"));
+
+    chrome.runtime.sendMessage({remove: false, link: alarmLink, time: alarmDate.getTime()});
+});
+
+function removeElement(id) {
+    var element = document.getElementById(id);
+    element.parentNode.removeChild(element);
+}
+
+function createListItem(link, date) {
+    var itemDiv = document.createElement("div");
+    itemDiv.className = "url-display";
+    itemDiv.id = link;
+
+    var extensionContent = document.getElementsByClassName("extension-content")[0];
+    extensionContent.appendChild(itemDiv);
+
+    var linkDiv = document.createElement("div");
+    linkDiv.className = "link-holder"
+    itemDiv.appendChild(linkDiv);
+
+    var linkElement = document.createElement("a");
+    linkElement.href = link;
+    linkElement.innerHTML = link;
+    linkDiv.appendChild(linkElement);
+
+    var dateElement = document.createElement("p");
+    dateElement.innerHTML = date;
+    itemDiv.appendChild(dateElement);
+
+    var removeButton = document.createElement("button");
+    removeButton.innerHTML = "Remove";
+    itemDiv.appendChild(removeButton);
+
+    removeButton.addEventListener("click", function(event) {
+        removeElement(link);
+        chrome.runtime.sendMessage({remove: true, link: link});
+    })
+}
+
+function generateUniqueLink(link) {
+    var finalLink = link;
+    var counter = 0;
+
+    while (document.getElementById(finalLink) != null) {
+        if (document.getElementById(finalLink + "#" + counter) == null) {
+            finalLink = finalLink + "#" + counter;
+            break;
+        } else {
+            counter += 1;
+        }
+    }
+
+    return finalLink;
+}
+
+document.getElementById("clear-button").addEventListener("click", function(event) {
+    event.preventDefault();
+    chrome.runtime.sendMessage("clear");
+    var extensionContent = document.getElementsByClassName("extension-content")[0];
+    extensionContent.innerHTML = "";
+})
+
+chrome.runtime.onMessage.addListener(function (message) {
+    createListItem(message.link, message.date);
 });
